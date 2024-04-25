eca04d40ba2f3517737d48ccb638376d5228b710
abhishektilkar <tilkarabhishek2000@gmail.com>
2024-04-26 03:32:54 +0530

    Added functionality update user and delete user

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -24,7 +24,37 @@ mod blog_app {
         Ok(())
     }    
 
-    
+    pub fn update_user(ctx: Context<UpdateUser>, name: String, avatar: String) -> Result<()> {
+
+        let user_account = &mut ctx.accounts.user_account;
+        user_account.name = name;
+        user_account.avatar = avatar;
+
+        Ok(())
+    }
+
+    pub fn delete_user(ctx: Context<DeleteUser>, name: String) -> Result<()> {
+        let user_account = &mut ctx.accounts.user_account;
+
+        msg!("Deleting a User account {} {}", user_account.name, name);
+        Ok(())
+    }
+
+    pub fn crate_post(ctx: Context<CreatePost>, title: String, content: String) -> Result<()> {
+
+        let post_account = &mut ctx.accounts.post_account;
+        let user_account = &mut ctx.accounts.user_account;
+        let authority = &mut ctx.accounts.authority;
+
+        post_account.id = user_account.last_post_id + 1;
+        user_account.last_post_id = post_account.id;
+        post_account.user = user_account.key();
+        post_account.title = title;
+        post_account.content = content;
+        user_account.post_count = user_account.post_count + 1;
+        post_account.authority = authority.key();
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -44,6 +74,40 @@ pub struct InitUser<'info> {
     )]
     pub authority: Signer<'info>,
     pub system_program: Program<'info,System>
+}
+
+#[derive(Accounts)]
+#[instruction(name: String, avatar: String)]
+pub struct UpdateUser<'info> {
+    #[account(
+        mut,
+        seeds = [USER_SEED, authority.key().as_ref()],
+        bump,
+        realloc = 8 + 32 + 8 + 8 + 4 + name.len() + 4 + avatar.len(),
+        realloc::payer = authority,
+        realloc::zero = true,
+    )]
+    pub user_account: Account<'info, UserAccount>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct DeleteUser<'info> {
+    #[account(
+        mut,
+        seeds = [USER_SEED, authority.key().as_ref()],
+        bump,
+        close = authority,
+    )]
+    pub user_account: Account<'info, UserAccount>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>
 }
 
 #[derive(Accounts)]
